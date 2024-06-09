eb3db6246af89e0383960f0cae69c0a01ed7a19c
Rachad-Alabi-ADEKAMBI <adekambirachad@gmail.com>
2024-06-09 20:21:26 +0100

    update

--- src/view/footer.php
+++ src/view/footer.php
@@ -73,7 +73,7 @@
 
 
 <!-- Back to Top -->
-<a href="#" class="btn btn-lg btn-blue btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+<a href="#app" class="btn btn-lg btn-blue btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
 
  <!-- JavaScript Libraries -->
  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
